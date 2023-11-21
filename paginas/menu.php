b50a1be686584f4f38b369a4fd4f9a0f528261b7
wiiska <128641437+wiiska@users.noreply.github.com>
2023-11-21 16:23:48 -0300

    Delete paginas directory

--- paginas/menu.php
+++
@@ -1,8 +0,0 @@
-<ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="AlunoList.php">Aluno</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-</ul>
